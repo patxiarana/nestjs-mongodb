fc18505f2040cf2efa62ff5096a382380d9a8463
unknown <patxiarana05@gmail.com>
2024-07-29 12:11:17 -0300

    get products and product

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
-
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Product } from './interfaces/product.interface';
+import { CreateProductDto } from './dto/product.dto';
 @Injectable()
-export class ProductService {}
+export class ProductService {
+
+    constructor(@InjectModel('Product') private readonly productModel: Model<Product>) {}
+
+    async getProducts() : Promise<Product[]> { 
+     const products =  await this.productModel.find()
+      return products; 
+    }
+
+   async getProduct(productID:string) : Promise<Product> { {
+         const product = await this.productModel.findById(productID)
+         return product;
+    }
+
+    createProduct(){
+
+    }
+
+    deleteProduct(){ 
+
+    }
+
+    updateProduct(){
+
+    }
+
+
+}
